2474f423ad03d8e2fed5996fb642c6aca77a2ba4
Noh June Seok <njs702@ajou.ac.kr>
2021-04-11 23:50:56 +0900

    Commit 3

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+
 public class BowlingGameTest {
 	private Game g;
 	
@@ -10,18 +11,22 @@ public class BowlingGameTest {
 	public void setUp() {
 		g = new Game();
 	}
-	
-	@Test
-	public void testGutterGame() {
-		for(int i=0;i<20;i++) 
-			g.roll(0);
-		assertEquals(0,g.score());
+	private void rollMany(int n,int pins) {
+		for(int i=0;i<n;i++)
+			g.roll(pins);
 	}
 	
 	@Test
+	public void testGutterGame() {
+		
+		rollMany(20,0);
+		assertEquals(0,g.score());
+	}
+	
+	
+	@Test
 	public void testAllOnes() {
-		for(int i=0;i<20;i++)
-			g.roll(1);
+		rollMany(20,1);
 		assertEquals(20,g.score());
 	}
 	
